02a6d868887350779ad8ecddbfe65e7630113a9e
antek-gzara-animations <58851540+Antek-Gzara-Animations@users.noreply.github.com>
2024-04-21 01:16:11 +0200

    added a bit of code for displaying all available serial ports

---
+++ ZSELteleturniej/SetupWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZSELteleturniej
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SetupWindow.xaml
+    /// </summary>
+    public partial class SetupWindow : Window
+    {
+        public List<string> Ports = new List<string>();
+
+        public SetupWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public void ShowData()
+        {
+            foreach (string element in Ports)
+            {
+                comList.Items.Add(element);
+            }
+        }
+    }
+}
